eb9b9e433f8a32f3a1f33ff440eb4a6392e78f5f
kofi arhin <kofiarhin69@gmail.com>
2025-07-04 13:56:52 +0100

    update: added a default endpoin

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const projectProfile = require("./config/project-profile.json");
 
 const app = express();
 
+app.get("/", (Req, res) => {
+  return res.json({ message: "hello world" });
+});
 app.get("/api/templates", (req, res) => {
   return res.json(projectProfile);
 });
