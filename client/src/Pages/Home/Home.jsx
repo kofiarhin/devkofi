efab125a5941f8d6af2ebe710ee073f5b76cb419
kofi arhin <kofiarhin69@gmail.com>
2025-07-07 22:01:04 +0100

    added audience section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Landing from "../../components/Landing/Landing";
 import About from "../../components/About/About";
+import Audience from "../../components/Audience/Audience";
 const Home = () => {
   return (
     <div className="container">
       <Landing />
       <About />
+      <Audience />
     </div>
   );
 };
---
+++ client/src/components/Audience/Audience.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./audience.styles.scss";
+
+const Audience = () => {
+  return (
+    <section id="audience" className="audience">
+      <h2 className="title">Who this is for</h2>
+      <div className="cards">
+        <div className="card">
+          <h3 className="card-title">
+            People who don't know how to build an app
+          </h3>
+          <p className="card-text">
+            Doesn't matter if you don't know how to code at all or are a
+            seasoned developer, I will help you learn how to build an app.
+          </p>
+          <a href="#get-started" className="cta">
+            Get started
+          </a>
+        </div>
+        <div className="card">
+          <h3 className="card-title">
+            People who don't know how to market their apps
+          </h3>
+          <p className="card-text">
+            I'll help you make social media content to grow your app on social
+            media. I'll be your social media marketing assistant.
+          </p>
+          <a href="#get-started" className="cta">
+            Get started
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Audience;
