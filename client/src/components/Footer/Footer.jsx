bba323e402f8eb2e6056fccbf1ae3c1a59f7b498
kofi arhin <kofiarhin69@gmail.com>
2025-07-10 06:52:57 +0100

    footer ui clean up

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+// src/components/Footer.jsx
 import "./footer.styles.scss";
+import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        Created by{" "}
-        <a href="https://github.com/kofiarhin" target="_blank">
-          Kofi Arhin
-        </a>{" "}
-      </p>
+      <p>© 2024 Your Company Name. All rights reserved.</p>
+      <div className="footer-socials">
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaTwitter />
+        </a>
+      </div>
     </footer>
   );
 };
