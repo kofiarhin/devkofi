4ecb27000c04941c4b71e1f42a06a02aa06bb655
kofi arhin <kofiarhin69@gmail.com>
2025-07-23 13:10:45 +0100

    update: changed landing page layout

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,20 +1,27 @@
-// src/pages/Landing.jsx
-import React from "react";
 import "./landing.styles.scss";
+import profileImage from "../../assets/img/profile-small.png";
 import { Link } from "react-router-dom";
-
-const Landing = () => (
-  <div id="landing">
-    <h1 className="heading">Mentorship That Turns Beginners into Builders</h1>
-    <p>
-      Get the guidance, support, and skills you need to build real-world
-      apps—and launch with confidence.
-    </p>
-
-    <Link className="cta" to="/mentorship">
-      Get Started
-    </Link>
-  </div>
-);
+const Landing = () => {
+  return (
+    <div id="landing">
+      <div className="landing-wrapper">
+        <div className="text-wrapper">
+          <h1 className="heading">
+            LAND A NEW CAREER WITH NEXT-LEVEL MENTORSHIP
+          </h1>
+          <p>
+            Yes, you can. Start your journey to a tech career in high-demand
+            fields like software engineering or data. With expert mentorship and
+            hands-on MERN stack training, you can be job-ready in just 6 months.
+          </p>
+        </div>
+        <div className="img-wrapper">
+          <img src={profileImage} alt="" />
+        </div>
+      </div>
+      <Link to="/mentorship"> Get Started!</Link>
+    </div>
+  );
+};
 
 export default Landing;
