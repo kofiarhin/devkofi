065523ff49a4f2773cb29ce26bd919e03bb7f090
kofi arhin <kofiarhin69@gmail.com>
2025-08-12 07:01:21 +0100

    ui clean up: typo mentorship

--- client/src/components/Landing/landingData.js
+++ client/src/components/Landing/landingData.js
@@ -2,4 +2,4 @@ export const text =
   "Start Your Tech Career in 6 Months. Master the MERN Stack, gain hands-on experience with AI tools, and accelerate your journey with expert mentorship to become fully job-ready in today’s high-demand tech industry.";
 
 export const title = "MERN STACK MENTORSHIP PROGRAMME 2025";
-export const subTitle = "MERN STACK mENTORSHIP PROGRAMME 2025";
+export const subTitle = "MERN STACK MENTORSHIP PROGRAMME 2025";
