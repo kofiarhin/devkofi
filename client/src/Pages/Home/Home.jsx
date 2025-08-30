df9d42be17c5fb6403e4fca27eca08e5440a4f81
kofi arhin <kofiarhin69@gmail.com>
2025-08-30 17:31:34 +0100

    testing live

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -13,7 +13,7 @@ import IntegrateAI from "../../components/IntegrateAi/IntegrateAi";
 const Home = () => {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1>We are live</h1>
       {/* <Landing /> */}
       {/* <Overview /> */}
       {/* <About /> */}
