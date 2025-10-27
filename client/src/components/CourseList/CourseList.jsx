33ff802c88900311252b584cce44bb0ce0e27fcc
kofi arhin <kofiarhin69@gmail.com>
2025-10-27 10:44:38 +0000

    update courses ui

--- client/src/components/CourseList/CourseList.jsx
+++ client/src/components/CourseList/CourseList.jsx
@@ -1,5 +1,6 @@
 import "./course-list.styles.scss";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSterlingSign } from "@fortawesome/free-solid-svg-icons";
 const CourseList = ({ courses = [] }) => {
   if (courses.length === 0) {
     return <p> no courses found!</p>;
@@ -9,11 +10,17 @@ const CourseList = ({ courses = [] }) => {
       {/* container */}
       <div className="container">
         {courses?.map((course, index) => {
+          console.log({ course });
           return (
             <div key={index} className="course-item">
               {" "}
               <img src={course?.media?.thumbnailUrl} />
               <h2> {course.name} </h2>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faSterlingSign} />
+                {course?.price.amount}{" "}
+              </p>
             </div>
           );
         })}
