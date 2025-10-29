c318242265f9d48628867b09e66a440887af729f
kofi arhin <kofiarhin69@gmail.com>
2025-10-29 10:43:35 +0000

    testing rebase

--- playground.js
+++ playground.js
@@ -2,7 +2,7 @@ const { fetchYoutubeVideos } = require("./server/utility/helper");
 
 const run = async () => {
   const result = await fetchYoutubeVideos();
-  console.log({ result });
+  console.log({ result, message: "thisis a test message" });
 };
 
 run();
