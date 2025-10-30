4f110c7f0c1a9b2236ededec9e583eb197290fa0
kofi arhin <kofiarhin69@gmail.com>
2025-10-30 18:25:36 +0000

    deployment fix

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 const DEV_API_URL = "http://localhost:5000";
-const PROD_API_URL = "https://devkofi-883f1d7b0ba0.herokuapp.com";
+const PROD_API_URL = "https://devkofi-api-82532bf8b693.herokuapp.com";
 
 const normalizeUrl = (url) => {
   if (typeof url !== "string") {
--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,4 +1,5 @@
-const baseUrl = process.env.API_BASE_URL || "https://api.devkofi.com";
+const baseUrl =
+  process.env.API_BASE_URL || "https://devkofi-api-82532bf8b693.herokuapp.com";
 
 module.exports = {
   baseUrl,
