8da5ec079268c221e61a9445f2cf61c016acd2a7
kofi arhin <kofiarhin69@gmail.com>
2025-11-12 08:28:00 +0000

    days work

--- client/src/Pages/Playground/Playground.jsx
+++ client/src/Pages/Playground/Playground.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import "./playground.styles.scss";
 import Upload from "../../components/Upload/Upload";
 const Playground = () => {
+  const [trapEmpty, setTrapEmpty] = useState(false);
+  console.log(trapEmpty);
+
   return (
     <div>
-      <Upload />
+      <div
+        id="playground"
+        onMouseEnter={() => setTrapEmpty(false)}
+        onMouseLeave={() => setTrapEmpty(true)}
+        className={trapEmpty ? "empty" : ""}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          veritatis omnis nesciunt fugiat reiciendis tempore in commodi libero
+          facere.
+        </p>
+      </div>
     </div>
   );
 };
