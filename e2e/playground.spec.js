aff34a72f8b5fb3ead3151fd12906708b3c97a7b
kofi arhin <kofiarhin69@gmail.com>
2025-11-13 08:27:45 +0000

    add test to courses page

--- e2e/playground.spec.js
+++ e2e/playground.spec.js
@@ -2,21 +2,34 @@ import { test, expect } from "@playwright/test";
 
 const BASE = "http://localhost:5174";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(BASE);
-});
-test.describe("home page test", () => {
-  test("passing test", async ({ page }) => {
-    const pageTitle = await page.title();
-    console.log({ pageTitle });
-    expect(pageTitle).toContain("DevKofi");
-  });
+// test.beforeEach(async ({ page }) => {
+//   await page.goto(BASE);
+// });
 
-  test("pricing is showing properly", async ({ page }) => {
-    await page.goto("http://localhost:5174"); // ✅ load your running site
-    await page.waitForSelector(".pricing .heading", { timeout: 10000 }); // ✅ wait for it to appear
-    const pricingElement = page.locator(".pricing .heading");
-    await expect(pricingElement).toBeVisible(); // ✅ assert visibility
-    await expect(pricingElement).toHaveText("Pricing"); // ✅ confirm it shows correct text
+// test.describe("home page test", () => {
+//   test("passing test", async ({ page }) => {
+//     const pageTitle = await page.title();
+//     console.log({ pageTitle });
+//     expect(pageTitle).toContain("DevKofi");
+//   });
+
+//   test("pricing is showing properly", async ({ page }) => {
+//     await page.goto("http://localhost:5174");
+//     await page.waitForSelector(".pricing .heading", { timeout: 10000 });
+//     const pricingElement = page.locator(".pricing .heading");
+//     await expect(pricingElement).toBeVisible();
+//     await expect(pricingElement).toHaveText("Pricing");
+//   });
+// });
+
+test.describe("Courses Page", () => {
+  test("passing courses test", async ({ page }) => {
+    await page.goto(`${BASE}/courses`);
+
+    // get list of courses
+    const courseItems = page.locator("#course-list .course-item");
+
+    await expect(courseItems).toHaveCount(4);
+    await page.waitForTimeout(5000);
   });
 });
