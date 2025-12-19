2d79776d092b9fd425b371afbe95e447ce303995
kofi arhin <kofiarhin69@gmail.com>
2025-12-19 11:14:08 +0000

    change favicon

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,42 +1,62 @@
 import "./header.styles.scss";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/auth/authSlice";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { toggleSideNav } from "../../redux/navigation/navigationSlice";
 
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
-
-  const dispath = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem("user");
-    dispath(logoutUser());
+    dispatch(logoutUser());
+    navigate("/login");
   };
+
   return (
     <header className="main-header">
-      <div className="container">
-        <Link to="/">
-          <h1 className="logo">DevKofi</h1>
+      <div className="container header-content">
+        <Link to="/" className="logo-container">
+          <h1 className="logo">
+            Dev<span>Kofi</span>
+          </h1>
         </Link>
 
-        <nav>
-          <FaBars className="menu" onClick={() => dispath(toggleSideNav())} />
-          <Link to="/">Home</Link>
+        <nav className="nav-links">
+          <Link to="/" className="nav-item">
+            Home
+          </Link>
           {user ? (
             <>
-              <Link to="/dashboard">Dashboard</Link>
-              <button onClick={handleLogout}>Logout</button>
+              <Link to="/dashboard" className="nav-item">
+                Dashboard
+              </Link>
+              <button className="btn-logout" onClick={handleLogout}>
+                Logout
+              </button>
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
-              <Link to="/register">Regiser</Link>
+              <Link to="/login" className="nav-item">
+                Login
+              </Link>
+              <Link to="/register" className="btn-cta">
+                Get Started
+              </Link>
             </>
           )}
         </nav>
+
+        <div className="mobile-actions">
+          <FaBars
+            className="menu-icon"
+            onClick={() => dispatch(toggleSideNav())}
+          />
+        </div>
       </div>
     </header>
   );
