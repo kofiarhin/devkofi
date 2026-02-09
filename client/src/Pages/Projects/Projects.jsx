8e2fa6fcb4aa30aadf041f8fdbaa4f7042409cf8
kofi arhin <kofiarhin69@gmail.com>
2026-02-09 07:26:22 +0000

    check if fetching works

--- client/src/Pages/Projects/Projects.jsx
+++ client/src/Pages/Projects/Projects.jsx
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 const Projects = () => {
   useEffect(() => {
     const fetchProjects = async () => {
-      const url = import.meta.env.VITE_API_URL || "http://localhost:5000";
-      const res = await fetch(url);
-      console.log(res.ok);
+      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+      const res = await fetch(`${API_URL}/api/projects`);
+      const data = await res.json();
+      console.log({ data });
     };
 
     fetchProjects();
