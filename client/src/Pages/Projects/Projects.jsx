559f5c3751d35c8c4b2868a7956549b11bfb2adf
kofi arhin <kofiarhin69@gmail.com>
2026-02-09 07:23:15 +0000

    fixing fetching issues

--- client/src/Pages/Projects/Projects.jsx
+++ client/src/Pages/Projects/Projects.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
 
 const Projects = () => {
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const url = import.meta.env.VITE_API_URL || "http://localhost:5000";
+      const res = await fetch(url);
+      console.log(res.ok);
+    };
+
+    fetchProjects();
+  }, []);
   return <div>Projects</div>;
 };
 
