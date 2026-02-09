bdb0de516cba65848b26c48207634bb3b1306ad8
kofi arhin <kofiarhin69@gmail.com>
2026-02-09 07:27:56 +0000

    debuging proeuctsion

--- client/src/Pages/Projects/Projects.jsx
+++ client/src/Pages/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const Projects = () => {
   useEffect(() => {
     const fetchProjects = async () => {
-      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
       const res = await fetch(`${API_URL}/api/projects`);
       const data = await res.json();
       console.log({ data });
