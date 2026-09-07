2f1398244e68b4892f9d5ab24907c45bb55cd933
Kofi Arhin <kofiarhin69@gmail.com>
2026-09-07 04:56:29 +0100

    feat(admin): support subscriber CRUD filters

--- client/src/hooks/queries/useNewsletterSubscribers.js
+++ client/src/hooks/queries/useNewsletterSubscribers.js
@@ -2,12 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
 import { getNewsletterSubscribers } from '../../services/adminService';
 
-const useNewsletterSubscribers = (page = 1) => {
+const useNewsletterSubscribers = (params = {}) => {
   const admin = useSelector((state) => state.auth.admin);
 
   return useQuery({
-    queryKey: ['newsletterSubscribers', page],
-    queryFn: () => getNewsletterSubscribers(page),
+    queryKey: ['newsletterSubscribers', params],
+    queryFn: () => getNewsletterSubscribers(params),
     enabled: !!admin,
   });
 };
